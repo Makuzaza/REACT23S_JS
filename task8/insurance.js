0f1d5a70e078add1009b4b8b58169ca4c3053e8e
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-09-08 19:58:47 +0300

    new

--- task8/insurance.js
+++ task8/insurance.js
@@ -4,8 +4,6 @@ let current;
 
 const insuranceRisk = () => {
     let score = 500;
-    let habits = '';
-    // define question and answer
     const age1 = document.querySelector('#age1');
     const age2 = document.querySelector('#age2');
     const age3 = document.querySelector('#age3');
@@ -29,27 +27,45 @@ let score_age;
 if (age1.checked) {
     score_age = score;
     console.log(score_age);
+    age = age1.value;
 } else if (age2.checked) {
     score_age = score + (score*0.1);
     console.log(score_age);
+    age = age2.value;
 } else if (age3.checked) {
     score_age = score + (score*0.3);
     console.log(score_age);
+    age = age3.value;
 } else if (age4.checked) {
     score_age = score + (score*0.6);
     console.log(score_age);
+    age = age4.value;
 } else if (age5.checked) {
     score_age = score + (score*1);
     console.log(score_age);
+    age = age5.value;
 } else if (age6.checked) {
     score_age = score + (score*1.5);
     console.log(score_age);
+    age = age6.value;
 } else if (age7.checked){
     score_age = score + (score*2.1);
     console.log(score_age)
+    age = age7.value;
 }
 
 let score_current = score_age;
+
+let checkboxes = document.querySelectorAll('input[name="current"]');
+let values = [];
+for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked == true) {
+        values.push(checkboxes[i].value);
+        score_current = score_current + (score_age*0.01);
+    }
+}
+console.log(values)
+console.log(score_current)
 
 // if (current1.checked) {
 //     score_current = score_current + (score_age*0.01);
@@ -67,107 +83,37 @@ let score_current = score_age;
 //     console.log(score_current);
 // }
 
-let checkboxes = document.querySelectorAll('input[name="current"]');
-let values = [];
-for (let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked == true) {
-        values.push(checkboxes[i].value);
-        score_current = score_current + (score_age*0.01);
+let score_goodHabits = score_age
+let score_gap = score_age*0.05
+let checkboxes_goodHabits = document.querySelectorAll('input[name="good_habits"]');
+let valuesHabits = [];
+for (let i = 0; i < checkboxes_goodHabits.length; i++) {
+    if (checkboxes_goodHabits[i].checked == true) {
+        valuesHabits.push(checkboxes_goodHabits[i].value);
+        score_goodHabits = score_goodHabits - score_gap
     }
 }
-console.log(values)
-console.log(score_current)
+console.log(valuesHabits)
+console.log(score_goodHabits)
 
-// let toppingCount = 0;
-// let addPrice = 0;
-
-// if (topping1.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = topping1.value;
-//     console.log(toppings);
-// } 
-// if (topping2.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + '' + topping2.value;
-//     console.log(toppings);
-// } 
-// if (topping3.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping3.value;
-//     console.log(toppings);
-// } 
-// if (topping4.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping4.value;
-//     console.log(toppings);
-// } 
-// if (topping5.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping5.value;
-//     console.log(toppings);
-// } 
-// if (topping6.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping6.value;
-//     console.log(toppings);
-// } 
-// if (topping7.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping7.value;
-//     console.log(toppings);
-// } 
-// if (topping8.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping8.value;
-//     console.log(toppings);
-// } 
-// if (topping9.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping9.value;
-//     console.log(toppings);
-// } 
-// if (topping10.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping10.value;
-//     console.log(toppings);
-// } 
-// if (topping11.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping11.value;
-//     console.log(toppings);
-// } 
-// if (topping12.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping12.value;
-//     console.log(toppings);
-// }
-// if (topping13.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping13.value;
-//     console.log(toppings);
-// }
-// if (topping14.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping14.value;
-//     console.log(toppings);
-// }
-// if (topping15.checked) {
-//     toppingCount = toppingCount + 1;
-//     toppings = toppings + ', ' + topping15.value;
-//     console.log(toppings);
-// }
-// console.log(toppingCount)
+let count = 0
+let score_badHabits = score_goodHabits
+let checkboxes_badHabits = document.querySelectorAll('input[name="bad_habits"]');
+let valuesBadHabits = [];
+for (let i = 0; i < checkboxes_badHabits.length; i++) {
+    if (checkboxes_badHabits[i].checked == true) {
+        valuesBadHabits.push(checkboxes_badHabits[i].value);
+        count++;
+        score_badHabits = score_goodHabits + score_gap*count;
+    }
+}
+console.log(valuesBadHabits)
+console.log(score_badHabits)
  
-// let sum = 0;
-// toppingCount = Number(toppingCount);
+let totalscore;
+totalscore = score_badHabits + (score_current - score_age)
 
-// if (toppingCount > 4) { 
-//     sum = size + delivery + (toppingCount-4)*0.5;
-//     answer.innerHTML = `Order for ${name} <br> Toppings: ${toppings} <br> Delivery: ${delivery_way} <br> Price: ${sum}€`;
-// } else if (toppingCount <= 4) {
-//     sum = size + delivery;
-//     answer.innerHTML = `Order for ${name} <br> Toppings: ${toppings} <br> Delivery: ${delivery_way} <br> Price: ${sum}€`;
-// } else {
-//         answer.textContent = 'Please, choose options';
-//     }
+answer.innerHTML = `${name}, You chose age: ${age} <br> Your current health: ${values} 
+<br> Your good health habits: ${valuesHabits} 
+<br> Unhealthy Habits: ${valuesBadHabits}<br> Your riskscore: ${totalscore}`;
 }
