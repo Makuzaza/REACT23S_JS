38681343bcda2ee447f747bb17b7b344c6d584af
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-09-19 13:19:23 +0300

    new

--- task11/main.js
+++ task11/main.js
@@ -1,6 +1,8 @@
 const backButton = document.querySelector('#backToTop');
 const mobButton = document.querySelector('.mobile');
 const nav = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a');
+const header = document.querySelector('header')
 
 window.onscroll = function() { scrollFunction() };
 
@@ -10,6 +12,12 @@ function scrollFunction() {
     } else {
         backButton.style.display = "none";
     }
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+       header.classList.add('bg');
+    } else {
+        header.classList.remove('bg');
+    }
 }
 
 const getToTop = () => {
@@ -18,6 +26,9 @@ const getToTop = () => {
 }
 
 const mobMenu = () => {
+for (const item of menuItems){
+    item.addEventListener('click', mobMenu)
+}
     if (nav.classList.contains('responsive')) {
         nav.classList.remove('responsive')
     } else {
