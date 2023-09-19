2bff5e7b8a72d97e89f293da0dd603ff8b3af950
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-09-19 10:13:35 +0300

    new

--- task9/pizza_objects.js
+++ task9/pizza_objects.js
@@ -1,14 +1,42 @@
-class Pizza {
-    name;
-    toppings = [];
-    basePrice = 0;
-    size = 'S';
+// class Pizza {
+//     name;
+//     toppings = [];
+//     basePrice = 0;
+//     size = 'S';
     
-    getPrice() {
+//     getPrice() {
         
+//     }
+
+// }
+
+class Pizza {
+    constructor(name, toppings) {
+      this.name = name;
+      this.toppings = toppings;
+      this.basePrice = 7.5;
     }
+  
+    getPrice() {
+      let totalPrice = this.basePrice;
+      const freeToppings = 4;
+      const additionalToppings = Math.max(0, this.toppings.length - freeToppings);
 
-}
-
-const americano = new Pizza();
-americano.name = 'americano'
+      for (let i = 0; i < additionalToppings; i++) {
+        totalPrice += 0.5;
+      }
+      return totalPrice;
+    }
+  }
+  
+  const myPizza = new Pizza("Pepperoni Pizza", [
+    "pepperoni",
+    "mushrooms",
+    "greenPeppers",
+    "onions",
+    "olives",
+  ]);
+  
+  const totalPrice = myPizza.getPrice();
+  console.log(`The price of ${myPizza.name} is €${totalPrice.toFixed(2)}`);
+  
