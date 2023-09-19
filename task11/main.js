1ae6c8560fbfdeb0cc7546b35c3530a3650ed1ae
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-09-19 11:03:49 +0300

    new

---
+++ task11/main.js
@@ -0,0 +1,10 @@
+const backButton = document.querySelector('#backToTop');
+
+const getToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
+
+}
+
+backButton.addEventListener('click', getToTop)
