c0f7adb6985e00825ed80efb5a8fc98204798ef3
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-09-27 13:04:08 +0300

    new

--- task7/timeout.js
+++ task7/timeout.js
@@ -1,15 +1,39 @@
-function wait(seconds, callback) {
-    setTimeout(callback, seconds*1000);
+const section = document.querySelector('section');
+const heading = document.querySelector('h3');
+const server = 'https://jsonplaceholder.typicode.com'
+const todo1URL = server + '/todos/1';
+function createApiURL(path) {
+    return server + path;
 }
 
-wait(2, () => {
-    console.log('two seconds');
-});
+fetch(createApiURL('/todos/1'))
+      .then(response => response.json())
+      .then(json => console.log(json))
 
-wait(1, () => {
-    console.log('three seconds');
-});
+async function getUser(id) {
+    const userURL = server + '/users/' + id;
+    const user = await fetch(userURL).then(response => response.json());
+    return user;
+}
 
-wait(1, () => {
-    console.log('four seconds');
-});
+async function getToDo(id, callback) {
+    const todoURL = createApiURL('/todos/' + id);
+    fetch(todoURL)
+     .then(response => response.json())
+     .then(json => callback(json));
+ }
+async function printUser(callback) {
+    let userTwo = await getUser(2);
+    heading.textContent = userTwo.name;
+    section.appendChild(callback(userTwo.address));
+}
+
+function createAddressBlock(address) {
+    const elem = document.createElement('div');
+    elem.innerHTML = `<p class='address'>
+    ${address.street} </br> ${address.suite} <br>
+    ${address.city}</p>`;
+    return elem;
+}
+
+printUser(createAddressBlock);
