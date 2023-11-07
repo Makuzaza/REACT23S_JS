945ce142383efe424ae017debc87b4cd9202b476
Makuzaza <maria.kuznetsova@edu.bc.fi>
2023-11-07 22:30:51 +0200

    added menu, links, mobile menu

--- html_css/10/main.js
+++ html_css/10/main.js
@@ -1,5 +1,8 @@
 const backButton = document.querySelector('#backToTop');
-
+const mobButton = document.querySelector('.mobile');
+const nav = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a');
+const header = document.querySelector('header');
 
 window.onscroll = function() { scrollFunction() };
 
@@ -16,6 +19,16 @@ const getToTop = () => {
     document.documentElement.scrollTop = 0;
 }
 
-
-
+const mobMenu = () => {
+    for (const item of menuItems) {
+        item.addEventListener('click', mobMenu);
+    }
+    if (nav.classList.contains('responsive')) {
+        nav.classList.remove('responsive')
+    } else {
+        nav.classList.add('responsive')
+    }
+}
+    
 backButton.addEventListener('click', getToTop)
+mobButton.addEventListener('click', mobMenu)
